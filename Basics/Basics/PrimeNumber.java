89a61424dbb04090fd10700506269656a27ddbfd
Medha V <medhaveeraiyan@gmail.com>
2026-06-16 21:45:34 +0530

    Add PrimeNumber program

---
+++ Basics/Basics/PrimeNumber.java
@@ -0,0 +1,22 @@
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int n = 29;
+        boolean isPrime = true;
+
+        if (n <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+            System.out.println(n + " is Prime");
+        else
+            System.out.println(n + " is Not Prime");
+    }
+}
