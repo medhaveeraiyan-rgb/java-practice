2d64fbe269da9f0004d06988b33570864c2ed8f0
Medha V <medhaveeraiyan@gmail.com>
2026-06-19 23:51:35 +0530

    Add InheritanceDemo with Animal and Dog classes

---
+++ Basics/Basics/OOP/OOP/InheritanceDemo.java
@@ -0,0 +1,20 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class InheritanceDemo {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+
+        d.sound(); // inherited method
+        d.bark();  // own method
+    }
+}
