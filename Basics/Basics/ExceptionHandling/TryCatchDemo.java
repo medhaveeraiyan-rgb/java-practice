1b98960a947505175022bfc7324d91453bb31bc3
Medha V <medhaveeraiyan@gmail.com>
2026-07-02 22:56:22 +0530

    Create TryCatchDemo.java

---
+++ Basics/Basics/ExceptionHandling/TryCatchDemo.java
@@ -0,0 +1,12 @@
+public class TryCatchDemo {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0;
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero.");
+        }
+
+        System.out.println("Program continues...");
+    }
+}
